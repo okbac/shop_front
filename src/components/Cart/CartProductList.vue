464a2c37c68db74e76578d37730a6a8b0bca7652
Chadli <okbachadli@gmail.com>
2025-01-16 13:59:07 +0100

    first commit

---
+++ src/components/Cart/CartProductList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import CartProduct from "./CartProduct.vue";
+
+defineProps({ cartList: Array,
+
+});
+</script>
+
+<template>
+  <div>
+    <CartProduct v-for="product of cartList" :product="product" />
+    <div class="grid">
+    
+  </div>
+    <div class="grid">
+    
+  </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Shop/Shop.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ShopProductList from "./ShopProductList.vue";
+
+// récupération des données transmises depuis App
+// nous récupérons un tableau de produits
+defineProps({
+  productList: Array,
+}); 
+
+
+//récupération de la methode
+const emit = defineEmits(['addProductToCart']);
+</script>
+
+<template>
+  <div class="p-20">
+    <ShopProductList :productList="productList" 
+    @add-product-to-cart="(productId) => emit('addProductToCart', productId)" />
+  </div>
+</template>
